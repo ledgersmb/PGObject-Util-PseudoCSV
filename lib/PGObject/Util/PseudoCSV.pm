334e8339525fccb2853e6eba710fbe03e6684e64
Chris Travers <chris@efficito.com>
2014-03-01 07:43:44 -0800

    Initial code done.  Probably doesn't work.  Now for testing and debugging.

--- lib/PGObject/Util/PseudoCSV.pm
+++ lib/PGObject/Util/PseudoCSV.pm
@@ -3,6 +3,7 @@ package PGObject::Util::PseudoCSV;
 use 5.006;
 use strict;
 use warnings;
+use PGObject;
 
 =head1 NAME
 
@@ -19,34 +20,152 @@ our $VERSION = '0.01';
 
 =head1 SYNOPSIS
 
-Quick summary of what the module does.
+This is a parser and constructor for PostgreSQL text representations of tuples
+and arrays.
 
-Perhaps a little code snippet.
+To parse:
 
-    use PGObject::Util::PseudoCSV;
+For a tuple, we'd typically:
 
-    my $foo = PGObject::Util::PseudoCSV->new();
-    ...
+   my @list = pseudocsv_parse($text_representation, @typelist);
+
+We can then arrange the hash as:
+
+   my $hashref = pseudocsv_to_hash(\@list, \@col_list);
+
+Which we can combine as:
+
+   my $hashref =  pseudocsv_to_hash(
+                     pseudocsv_parse($text_representation, @typelist),
+                     \@col_list
+   );
+
+For an array we specify a single type to the parser:
+
+   my @list = pseudocsv_parse($text_representation, $type);
+
+=head1 DESCRIPTION
+
+PostgreSQL can represent tuples and arrays in a text format that is almost like
+CSV.  Unfortunately this format has a number of gotchas which render existing 
+CSV-parsers useless.  This module provides basic parsing functions to other
+programs for db interface purposes.  With this module you can both parse 
+pseudocsv representations of tuples and arrays and you can create them from a 
+list.
+
+The API's here assume you send one (and only one) pseudo-csv record to the API 
+at once.  These may be nested, so a single tuple can contain arrays of tuples 
+which can contain arrays of tuples ad infinitum but the parsing only goes one
+layer deep tuple-wise so that handlin classes have an opportunity to re-parse
+with appropriate type information. Naturally this has performance implications,
+so depth in SQL structures passed should be reasonably limited.
 
 =head1 EXPORT
 
-A list of functions that can be exported.  You can delete this section
-if you don't export anything, such as for a purely object-oriented module.
+=over
+
+=item pseudocsv_to_hash
+
+=item pseudocsv_parse
+
+=item to_pseudocsv
+
+=back
+
+=cut
+
+use parent 'Exporter';
+
+@EXPORT = qw(pseudocsv_to_hash pseudocsv_parse);
 
 =head1 SUBROUTINES/METHODS
 
-=head2 function1
+=head2 pseudocsv_parse
+
+This does a one-level deep parse of the pseudo-csv, with additional levels in
+arrays.  When a tuple is encountered it is instantiated as its type but a 
+subarray is parsed for more entities.
 
 =cut
 
-sub function1 {
+sub pseudocsv_parse {
+    my ($csv, $type, $registry) = @_;
+    $registry ||= 'default';
+    my @returnlist = ();
+    while (my $val = _parse(\$csv)){
+        my $in_type = $type;
+        $in_type = shift @$type if ref @type eq ref [];
+        $val =~ s/""/"/g;
+        push @returnlist, PGObject::process_type($val, $type, $registry);
+    }
+    return @returnlist if wantarray;
+    return \@returnlist;
 }
 
-=head2 function2
+# _parse is the private method which does the hard work of parsing.
+
+sub _parse {
+    my ($csvref) = @_;
+    my $retval;
+    if ($$csvref =~ /^"/){ # quoted string
+       $$csvref =~ s/^"(.*[^"]("{2})*)",//;
+       $retval = $1;
+    } else {
+       $$csvref =~ s/^([^,]*),//;
+       $retval = $1;
+       $retval = undef if $retval =~ /^null$/i;
+    }
+    if ($retval =~ s/^\{(.*)\}$/$1/){
+        my $listref = [];
+        push @$listref, _parse(\$retval) while $retval;
+        $retval = $listref;
+    }
+    return $retval;
+}
+
+=head2 pseudocsv_tohash($coldata, $colnames)
+
+Takes an arrayref of column data and an arrayref of column names and returns 
+a hash.
 
 =cut
 
-sub function2 {
+sub pseudocsv_tohash {
+    my ($cols, $colnames) = @_;
+    my $hash = {};
+    for my $col (@cols) {
+        my $colname = shift @$colnames;
+        last unless defined $colname;
+        $hash->{$colname} = $col;
+    }
+    return %$hash if wantarray;
+    return $hash;
+}
+
+=head2 to_pseudocsv($datalist, $is_tuple)
+
+Takes a list of data and an is_tuple argument and creates a pseudocsv.
+
+=cut
+
+sub to_pseudocsv {
+    my ($list, $is_tuple) = @_;
+    croak 'First arg must be an arrayref' unless ref $list;
+    my $csv = "";
+    for my $item (@$list){
+        $csv .= ',' if $csv;
+        $csv .= 'NULL' && next unless defined $item;
+        if (ref $item eq ref []){
+             my $val = to_pseudocsv($item, 0);
+             $val = qq{"$val"} if $is_tuple;
+             $csv .= $val;
+             next;
+        }
+        $item =~ s/"/""/;
+        $item = qq{"$item"} if $item =~ /(^null$|[",{})/;
+    }
+    return qq|($csv)| if $is_tuple;
+    return qq|{$csv}|;
 }
 
 =head1 AUTHOR
