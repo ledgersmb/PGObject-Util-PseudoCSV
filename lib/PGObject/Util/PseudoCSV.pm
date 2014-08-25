7904b4df58392f9709b6362b54907d6e16e44dc8
einhverfr <none@none>
2014-08-25 01:05:44 -0700

    Minor doc changes

--- lib/PGObject/Util/PseudoCSV.pm
+++ lib/PGObject/Util/PseudoCSV.pm
@@ -12,11 +12,11 @@ PGObject::Util::PseudoCSV - Tuple/Array parsing and serialization for PGObject
 
 =head1 VERSION
 
-Version 1.0.2
+Version 1.0.3
 
 =cut
 
-our $VERSION = '1.0.2';
+our $VERSION = '1.0.3';
 
 
 =head1 SYNOPSIS
